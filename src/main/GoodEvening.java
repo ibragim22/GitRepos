e4ba6fbd762c8af737dac65296102b987f500f7a
ibragimbeksamiev <ibragimbeksamiev@10.1.10.91>
2018-03-10 13:33:01 -0600

    initial commit

---
+++ src/main/GoodEvening.java
@@ -0,0 +1,9 @@
+package main;
+
+public class GoodEvening {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello from the other side");
+	}
+}
---
+++ src/main/GoodMorning.java
@@ -0,0 +1,9 @@
+package main;
+
+public class GoodMorning {
+
+	
+	public void get() {
+		System.out.println("take it");
+	}
+}
